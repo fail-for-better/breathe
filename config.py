b7f8a4fbd23dc913216783f2e994f1b31dd9cf07
fail-for-better <fengziqne@163.com>
2021-11-27 21:54:39 +0800

    update mysql password

--- config.py
+++ config.py
@@ -6,11 +6,11 @@ class Config(object):
     SQL_CONIFG = {
         'type':  os.getenv('MYSQL_TYPE', 'mysql') ,
         'driver': os.getenv('DRIVER ', 'pymysql'),
-        'username': os.getenv('DB_USER', 'root'),
-        'password': parse.quote_plus(os.getenv('DB_PASSWORD', 'dandan@777')),
-        'host': os.getenv('DB_HOST', '115.29.195.32'),
-        'port': os.getenv('DB_PORT', '3306'),
-        'database': os.getenv('DB_NAME', 'sunshine')
+        'username': os.getenv('DB_USER', ''),
+        'password': parse.quote_plus(os.getenv('DB_PASSWORD', '')),
+        'host': os.getenv('DB_HOST', ''),
+        'port': os.getenv('DB_PORT', 3306),
+        'database': os.getenv('DB_NAME', '')
     }
     con_str = '{type}+{driver}://{username}:{password}@{host}:{port}/{database}'
     SQLALCHEMY_DATABASE_URI = os.getenv('DB_URL') or con_str.format(**SQL_CONIFG)
